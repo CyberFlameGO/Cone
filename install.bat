3c0273113e9ec7bc4067c07f39b532b6a16bc4fa
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-12-01 12:03:46 +0100

    Improve install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 TITLE Cone Installer
 
+:: Ensure Administrator Privileges
 NET SESSION 1>NUL 2>NUL
 IF NOT %errorLevel% == 0 (
 	ECHO Requesting elevation...
@@ -8,12 +9,14 @@ IF NOT %errorLevel% == 0 (
 	EXIT
 )
 
+:: Parameters
 SET CONE_VERSION=0.10.3
 SET ARCH=x86
 IF %processor_architecture% == AMD64 (
 	SET ARCH=x64
 )
 
+:: Check PHP Installation
 CD %ProgramFiles%
 WHERE php.exe > php.txt
 IF %errorLevel% == 0 (
@@ -21,6 +24,7 @@ IF %errorLevel% == 0 (
 	GOTO phpinstalled
 )
 
+:: Install PHP
 CLS
 SET PHP_VERSION=7.4.0
 ECHO Downloading PHP %PHP_VERSION%...
@@ -30,7 +34,6 @@ IF NOT EXIST "%tmp%\php.zip" (
     PAUSE > NUL
     EXIT
 )
-
 ECHO Unpacking PHP...
 MKDIR "%tmp%\PHP %PHP_VERSION%"
 ECHO Set s = CreateObject("Shell.Application") > tmp.vbs
@@ -39,7 +42,6 @@ cscript //nologo tmp.vbs
 DEL tmp.vbs
 DEL %tmp%\php.zip
 MOVE "%tmp%\PHP %PHP_VERSION%" .
-
 ECHO Installing PHP...
 SET php=%cd%\PHP %PHP_VERSION%\php.exe
 IF NOT EXIST "%php%" (
@@ -48,27 +50,33 @@ IF NOT EXIST "%php%" (
     EXIT
 )
 SET PATH=%cd%\PHP %PHP_VERSION%\;%PATH%
+REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /F /V PATH /T REG_SZ /D "%PATH%"
 
 :phpinstalled
 DEL php.txt
+:: PHP is now guaranteed to be installed
 
+:: Configure PHP
 "%php%" -r "if(php_ini_loaded_file() === false) { $dir = dirname(shell_exec('where php')); file_put_contents($dir.'/php.ini', file_get_contents($dir.'/php.ini-development').\"\nextension_dir=\\\"$dir\\ext\\\"\"); }"
 "%php%" -r "file_put_contents(php_ini_loaded_file(), str_replace(';extension=openssl', 'extension=openssl', file_get_contents(php_ini_loaded_file())));"
 
-ECHO Downloading Cone v%CONE_VERSION%...
+:: Create Cone Folder
 IF NOT EXIST Hell.sh\ MKDIR Hell.sh
 CD Hell.sh
 IF NOT EXIST Cone\ MKDIR Cone
 CD Cone
 IF EXIST Cone.zip DEL Cone.zip
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; Invoke-WebRequest https://github.com/getcone/Cone/archive/v%CONE_VERSION%.zip -UseBasicParsing -OutFile %tmp%\Cone.zip"
 
+:: Download Cone
+ECHO Downloading Cone v%CONE_VERSION%...
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; Invoke-WebRequest https://github.com/getcone/Cone/archive/v%CONE_VERSION%.zip -UseBasicParsing -OutFile %tmp%\Cone.zip"
 IF NOT EXIST "%tmp%\Cone.zip" (
     ECHO Failed to download Cone.
     PAUSE > NUL
     EXIT
 )
 
+:: Unpack Cone
 IF EXIST _update_ (
 	ECHO Updating Cone...
 ) ELSE (
@@ -88,19 +96,21 @@ IF EXIST path\cone DEL path\cone
 ECHO #!/bin/bash > path\cone
 ECHO cone.bat "$@" >> path\cone
 RMDIR /S /Q %tmp%\Cone-%CONE_VERSION%
+
 IF EXIST _update_ (
+    :: Finish Update
 	DEL _update_
 	START cmd /k "CLS & DEL %tmp%\Cone_update.bat & cone update"
 	EXIT
 )
-"%php%" -r "exit(strpos(getenv('PATH'), '%cd%\path\;') !== false ? 1 : 0);"
-IF %errorLevel% == 0 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /F /V PATH /T REG_SZ /D "%cd%\path\;%PATH%"
-:: Setting a temporary dummy variable so setx will broadcast WM_SETTINGCHANGE so the PATH changes are reflected without needing a restart.
+
+:: Add Cone to PATH
+REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /F /V PATH /T REG_SZ /D "%cd%\path\;%PATH%"
 SETX /m DUMMY ""
 REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /F /V DUMMY
 
+:: Finish Install
 "%php%" src/cli.php update --post-install
-
 ECHO Cone is now installed.
 ECHO Get started by opening Command Prompt (as Administrator) and typing "cone help".
 PAUSE > NUL
